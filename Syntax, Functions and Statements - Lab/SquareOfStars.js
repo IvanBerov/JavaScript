f70ef1d80bf25051d2f950677badc4a56d045709
Ivan Berov <ivanberov3@gmail.com>
2022-05-23 12:16:41 +0200

    prints a rectangle made of stars

---
+++ Syntax, Functions and Statements - Lab/SquareOfStars.js
@@ -0,0 +1,15 @@
+function squareStars(x = 5){
+
+    let str = '';
+
+    for (let i = 0; i < x; i++) {
+        for (let j = 0; j < x; j++) {
+            str += `* `;
+        }
+        str += '\r\n';
+    }
+
+    return str;
+}
+
+console.log(squareStars(7));
