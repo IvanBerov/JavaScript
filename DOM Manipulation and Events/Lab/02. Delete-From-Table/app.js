b25674a80e90ad3ee938d69218442b4b6d9fd055
Ivan Berov <ivanberov3@gmail.com>
2022-07-11 23:30:17 +0200

    some dome exercise

--- DOM Manipulation and Events/Lab/02. Delete-From-Table/app.js
+++ DOM Manipulation and Events/Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,31 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let emailInputElement =  document.querySelector('input[name="email"]');
+
+    let emailCellsWeHave = document.querySelectorAll('tr td:nth-of-type(2)');
+
+    let resultElement = document.getElementById('result');
+
+    let emailElements = Array.from(emailCellsWeHave);
+
+    let targetElement = emailElements.find(x => x.textContent == emailInputElement.value);
+
+    if(targetElement){
+        targetElement.parentElement.remove();
+
+        resultElement.textContent = 'Deleted.'
+    }else{
+        resultElement.textContent = 'Not found.'
+    }
 }
+
+// Applies for NodeList and HTMLCollection
+// for (const email of emailCellElements) {
+    
+// }
+
+// Applies only for NodeList
+// emailCellElements.forEach(0);
+
+// The old way of deleting
+// targetElement.parentNode.removeChild(targetElement);
+// targetElement.parentNode.parentNode.removeChild(targetElement.parentNode);
