0df4c8c7a526dd1302864246c16988d72e2670ba
Ivan Berov <ivanberov3@gmail.com>
2022-07-13 14:58:51 +0200

    dom

--- DOM Manipulation and Events/Lab/04. Mouse-Gradient/app.js
+++ DOM Manipulation and Events/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,20 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
-}
+
+    let gradientBox = document.getElementById("gradient-box");
+    let result = document.getElementById("result");
+  
+    gradientBox.addEventListener("mousemove", gradientMovement);
+    gradientBox.addEventListener("mouseout", gradientOut);
+  
+    function gradientMovement(e) {
+
+      let percent = e.offsetX / (e.target.clientWidth - 1);
+      percent = Math.trunc(percent * 100);
+
+      result.textContent = `${percent}%`;
+    }
+  
+    function gradientOut(e) {
+      result.textContent = "";
+    }
+  }
