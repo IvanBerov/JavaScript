5c82f7f1f893134867f33c056fd0c0883929d498
Ivan Berov <ivanberov3@gmail.com>
2022-07-13 11:11:53 +0200

    dom event Manipulation

--- DOM Manipulation and Events/Lab/03. Add-Delete/app.js
+++ DOM Manipulation and Events/Lab/03. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    //TODO...
+    let itemsElement = document.getElementById('items');
+    let inputElement = document.getElementById('newItemText');
+
+    let newElement =  document.createElement('li');
+    newElement.textContent = inputElement.value;
+    inputElement.value = '';
+
+    let deleteElement = document.createElement('a');
+    deleteElement.href = '#';
+    deleteElement.textContent = '[Delete]';
+
+    deleteElement.addEventListener('click', (e) => {
+        e.currentTarget.parentElement.remove();
+    })
+
+    newElement.appendChild(deleteElement);
+    itemsElement.appendChild(newElement);
 }
