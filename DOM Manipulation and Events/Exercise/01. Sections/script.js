6a6725a9e67c9f8fbf90b563dd9fa102697d4bfe
Ivan Berov <ivanberov3@gmail.com>
2022-07-15 13:37:35 +0200

    dom exercise

--- DOM Manipulation and Events/Exercise/01. Sections/script.js
+++ DOM Manipulation and Events/Exercise/01. Sections/script.js
@@ -1,4 +1,24 @@
 function create(words) {
    
-   
+    let parentElement = document.getElementById('content');
+    let elements = words;
+
+    for (let i = 0; i < elements.length; i++) {
+
+        let div = document.createElement('div');
+        let p = document.createElement('p');
+
+        let text = document.createTextNode(elements[i]);
+
+        p.appendChild(text);
+        div.appendChild(p);
+
+        p.style.display = 'none';
+
+        div.addEventListener('click', function(e){
+           e.target.children[0].style.display = 'block'
+        });
+
+        parentElement.appendChild(div)
+    }
 }
