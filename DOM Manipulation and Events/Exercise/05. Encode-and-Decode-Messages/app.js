5fc894dca7cbcbc440d780902613acb000d9269e
Ivan Berov <ivanberov3@gmail.com>
2022-08-01 19:27:17 +0300

    dom exercise

--- DOM Manipulation and Events/Exercise/05. Encode-and-Decode-Messages/app.js
+++ DOM Manipulation and Events/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,40 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let buttons = document.getElementsByTagName('button');
+    let text = document.getElementsByTagName('textarea');
+
+    let encodeButtonEl = buttons[0];
+    encodeButtonEl.addEventListener('click', encode);
+
+    let decodeButtonEl = buttons[1];
+    decodeButtonEl.addEventListener('click', decode);
+
+    let decodeTextArea = text[1];
+
+    function encode(){
+
+        let encodeTextArea = text[0];
+        
+        const message = encodeTextArea
+            .value
+            .split('')
+            .map((x) => x.charCodeAt(0) + 1)
+            .map((x) => String.fromCharCode(x))
+            .join('');
+
+            encodeTextArea.value = '';
+            
+            decodeTextArea.value = message;
+    }
+
+    function decode(){
+
+        const message = decodeTextArea
+        .value
+        .split('')
+        .map((x) => x.charCodeAt(0) - 1)
+        .map((x)=>String.fromCharCode(x))
+        .join(''); 
+
+        decodeTextArea.value = message;
+    }
 }
