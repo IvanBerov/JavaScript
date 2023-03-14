32b58a0658af1b5ed9285f6e91715d7cc57954bc
Ivan Berov <ivanberov3@gmail.com>
2023-03-14 13:39:50 +0100

    bus schedule exercise

--- JS Applications/JS-Applications-Asynchronous-Programming-Exercise/02.Bus-Schedule/app.js
+++ JS Applications/JS-Applications-Asynchronous-Programming-Exercise/02.Bus-Schedule/app.js
@@ -1,11 +1,32 @@
 function solve() {
 
+    let infoElement = document.querySelector('.info');
+    let departBtn = document.getElementById('depart');
+    let arriveBtn = document.getElementById('arrive');
+
+    let busStop = {
+        next: 'depot'
+    }
+
     function depart() {
-        console.log('Depart TODO...');
+        departBtn.disabled = true
+        let url = `http://localhost:3030/jsonstore/bus/schedule/${busStop.next}`
+        console.log(url)
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            busStop = JSON.parse(JSON.stringify(data)); // no ref
+            infoElement.textContent = `Next stop ${busStop.name}`
+        })
+        .catch(err => console.log(err))
+
+        arriveBtn.disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoElement.textContent = `Arriving at ${busStop.name}`
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
