c05f3ceaba553e6108f5c5638873ff8b141549d7
Ivan Berov <ivanberov3@gmail.com>
2023-08-30 15:32:41 +0200

    add home, login and register

--- JS Applications/MusicApp/src/api/user.js
+++ JS Applications/MusicApp/src/api/user.js
@@ -2,32 +2,40 @@ import { clearUserData, setUserData } from "../util.js";
 import { get, post } from "./api.js";
 
 const endpoints = {
-    'login' : 
-    'register' : 
-    'logout' :
-}
+  login: "/users/login",
+  register: "/users/register",
+  logout: "/users/logout",
+};
 
 export async function login(email, password) {
-    const { _id, email: resultEmail, accessToken } = await post(endpoints.login, { email, password });
+  const {
+    _id,
+    email: resultEmail,
+    accessToken,
+  } = await post(endpoints.login, { email, password });
 
-    setUserData({
-        _id,
-        email: resultEmail,
-        accessToken
-    });
+  setUserData({
+    _id,
+    email: resultEmail,
+    accessToken,
+  });
 }
 
 export async function register(email, password) {
-    const { _id, email: resultEmail, accessToken } = await post(endpoints.logout, { email, password });
+  const {
+    _id,
+    email: resultEmail,
+    accessToken,
+  } = await post(endpoints.logout, { email, password });
 
-    setUserData({
-        _id,
-        email: resultEmail,
-        accessToken
-    });
+  setUserData({
+    _id,
+    email: resultEmail,
+    accessToken,
+  });
 }
 
 export async function logout() {
-    get(endpoints.logout);
-    clearUserData();
+  get(endpoints.logout);
+  clearUserData();
 }
--- JS Applications/MusicApp/src/app.js
+++ JS Applications/MusicApp/src/app.js
@@ -1,6 +1,23 @@
-const main = "";
+import { page, render } from "./lib.js";
+import { getUserData } from "./util.js";
+import { showHome } from "./views/homeView.js";
+import { showLogin } from "./views/login.js";
+import { updateNav } from "./views/nav.js";
+import { showRegister } from "./views/register.js";
+
+const main = document.getElementById("main-content");
 
 page(decorateContext);
+
+page("/", showHome);
+page("/home", showHome);
+page("/login", showLogin);
+page("/register", showRegister);
+page("/catalog", () => console.log("catalogView"));
+page("/create", () => console.log("createView"));
+page("/detail/:id", () => console.log("detailView"));
+page("/edit/:id", () => console.log("editView"));
+page("/search", () => console.log("searchView"));
 
 updateNav();
 page.start();
